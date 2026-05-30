c95cb33f8131b90b587ebdd05e90fb7907a718fd
aarna ✨ <82003715+AarnaAgrawal@users.noreply.github.com>
2026-05-29 17:52:54 -0700

    Update popup.js

--- gmail/popup.js
+++ gmail/popup.js
@@ -2,7 +2,7 @@ document.getElementById("button").addEventListener("click", () => {
     const email = document.getElementById("email").value.toLowerCase();
     const curseWords = ["shit", "ass"];
     const found = curseWords.some(word =>
-        new RegExp('\\b${word}\\b', "i").test(email)
+        new RegExp(`\\b${word}\\b`, "i").test(email)
         );
     const result = document.getElementById("result");
     if (found) {
